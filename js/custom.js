cf024af62bf87c7b50a1cd6885cf1abbf37cf0c3
VedantCMS <vedantgupta.cms@gmail.com>
2025-04-05 10:37:55 +0530

    My Second Push

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+let a = 25;
+let b = 35;
+ let c = a + b;
+ console.log("The reaulktr of Two number is = ", c);
+
+ let myArrw = ["Vedant", "Gupta", 30, "Frontend Developer"];
+ let yourArw = myArrw[2];
+
+ console.log(yourArw);
