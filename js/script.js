47b9f0256add771fe6a3e49b46522041df3ba340
VedantCMS <vedantgupta.cms@gmail.com>
2025-04-08 17:03:11 +0530

    gt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('scroll', function () {
+
+        if (window.scrollY > 200) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+
 $(document).ready(function() {
     $("#news-slider").owlCarousel({
         autoplay:true,
@@ -55,6 +72,36 @@ $(document).ready(function() {
 
                 992 : {
                     items: 3
+                }
+            }
+    });
+});
+
+$(document).ready(function() {
+    $(".alfaSlider").owlCarousel({
+        autoplay:true,
+            smartSpeed:300,
+            loop:true,
+            autoplayHoverPause:true,
+            mouseDrag: false,
+            dots: false,
+            nav: true,
+            navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+            responsive: {
+                0 : {
+                    items: 1
+                },
+
+                400 : {
+                    items: 1
+                },
+
+                600 : {
+                    items: 1
+                },
+
+                992 : {
+                    items: 1
                 }
             }
     });
